3f487d587ecfcbc6c49756d8859425c71aba8185
kevinstrax <847967921@qq.com>
2025-10-12 18:56:59 +0800

    Updates

---
+++ assets/Clara-DPWKtHqt.js
@@ -0,0 +1,1 @@
+import{H as e}from"./HelloWorld-nGk_Ls_N.js";import{e as r,J as a,u as o,j as t,L as n}from"./404Redirect-DObYdcFF.js";import"./index-DygtXZR8.js";const c=r({__name:"Clara",setup(s){return(l,m)=>(t(),a(e,{vtuber:o(n).AKATSUKI_CLARA.name},null,8,["vtuber"]))}});export{c as default};

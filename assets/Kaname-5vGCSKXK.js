6b7f0dcca83c508424f1e78bdc8e271d4ff45bc2
kevinstrax <847967921@qq.com>
2025-10-18 00:18:40 +0800

    Updates

---
+++ assets/Kaname-5vGCSKXK.js
@@ -0,0 +1,1 @@
+import{e,J as r,j as a,u as o,L as t}from"./404Redirect-CTXHqS89.js";import{H as n}from"./HelloWorld-CJljPHp_.js";import"./index-BvGTnd44.js";const l=e({__name:"Kaname",setup(s){return(m,p)=>(a(),r(n,{vtuber:o(t).TOKIWA_KANAME.name},null,8,["vtuber"]))}});export{l as default};
